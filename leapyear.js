f586eefb68b8cbe4936c625bd0b21888c95846ae
ridethewave42 <natebrwn3@gmail.com>
2023-10-26 10:27:54 +0200

    first commit

---
+++ leapyear.js
@@ -0,0 +1,15 @@
+function listLeap(year) {
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return "leap year";
+            } else {
+                return "Not a leap year";
+            }
+        } else {
+            return "leap year";
+        }
+    } else {
+        return "Not a leap year";
+    }
+}
